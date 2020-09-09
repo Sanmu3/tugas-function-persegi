2cd1e735f9b832712d984e31883fe17ba214cee1
Sanmu3 <mujahidmuslim0@gmail.com>
2020-09-09 16:07:52 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var panjang = 6
+var lebar = 8
+function persegiPanjang(panjang , lebar) {
+    var luas ;
+    luas = panjang * lebar;
+
+    return luas;
+}
+
+document.write('panjang : ' + panjang + '<br/>')
+document.write('<br/>lebar : ' + lebar + '<br/>')
+document.write('<br/>luas persegi panjang : ' + persegiPanjang(6,8))
